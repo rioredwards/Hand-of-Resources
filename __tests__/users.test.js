d41471a70eb82d32962c7c1ec82f8f45fd6b652d
Rio Edwards <rioredwards@gmail.com>
2022-11-11 13:57:18 -0800

    Tests Passing! Adds Index route for users

---
+++ __tests__/users.test.js
@@ -0,0 +1,48 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('users routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /users should return a list of users', async () => {
+    const resp = await request(app).get('/users');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "email": "jlefriec0@feedburner.com",
+          "id": "1",
+          "username": "tguinan0",
+        },
+        Object {
+          "email": "sducker1@nationalgeographic.com",
+          "id": "2",
+          "username": "ctixall1",
+        },
+        Object {
+          "email": "pdagostini2@smh.com.au",
+          "id": "3",
+          "username": "bvannucci2",
+        },
+        Object {
+          "email": "ajohnsey3@paypal.com",
+          "id": "4",
+          "username": "bandrieu3",
+        },
+        Object {
+          "email": "ebalcon4@elpais.com",
+          "id": "5",
+          "username": "dedy4",
+        },
+      ]
+    `);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
