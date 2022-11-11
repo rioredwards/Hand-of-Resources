629d39e5e9faa04154b7662f430245aa588f7152
Rio Edwards <rioredwards@gmail.com>
2022-11-10 16:30:13 -0800

    Tests Passing! Add Index for Colors

---
+++ __tests__/colors.test.js
@@ -0,0 +1,48 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('colors routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /colors should return a list of colors', async () => {
+    const resp = await request(app).get('/colors');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "is_my_favorite": false,
+          "name": "Purple",
+        },
+        Object {
+          "id": "2",
+          "is_my_favorite": false,
+          "name": "Red",
+        },
+        Object {
+          "id": "3",
+          "is_my_favorite": false,
+          "name": "Pink",
+        },
+        Object {
+          "id": "4",
+          "is_my_favorite": true,
+          "name": "Blue",
+        },
+        Object {
+          "id": "5",
+          "is_my_favorite": false,
+          "name": "Orange",
+        },
+      ]
+    `);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
